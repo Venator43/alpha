40bf7ef2ee5f9f0584519f915704bcfa82d715ee
Fabian <38877113+Venator43@users.noreply.github.com>
2023-03-28 02:59:12 +0700

    remove api key

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
+from .type_processor import type_processor
 
 app = FastAPI()
+procesor = type_processor("C:/Users/ASUS/Documents/Program/Python/AI/Experimental/alpha/config/test2.yml")
 
 @app.get("/{command_type}}/{command}")
 def read_root(command_type: str, command: str):
-    return {"Hello": "World"}
+    return procesor.run(command_type, command)
--- type/basic_command.py
+++ type/basic_command.py
@@ -3,7 +3,7 @@ import json
 from . import command_interface
 
 class basic_command(command_interface.command_interface):
-	def __init__(self, API_KEY="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", API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom", prompt="test"):
+	def __init__(self, API_KEY, API_URL, prompt):
 		self.API_URL = API_URL
 		self.headers = {"Authorization": f"Bearer {API_KEY}"}
 		self.prompt = 'Mohon nyalakan lampu atas\n>{"device_type" : "lamp", "command" : "on", "device_identifier" : "atas"}[EOS]\nMatikan lampu kamar depan\n>{"device_type" : "lamp", "command" : "off", "device_identifier" : "kamar depan"}[EOS]\nMatikan lampu depan\n>{"device_type" : "lamp", "command" : "off", "device_identifier" : "depan"}[EOS]\nTolong nyalakan komputer di dapur\n>{"device_type" : "PC", "command" : "on", "device_identifier" : "dapur"}[EOS]\nNyalakan TV di tempat tidur\n>{"device_type" : "TV", "command" : "on", "device_identifier" : "Tempat Tidur"}[EOS]\nMatikan microwave dapur\n>{"device_type" : "microwave", "command" : "off", "device_identifier" : "dapur"}[EOS]\ntolong nyalakan device nomer satu\n>{"device_type" : "device", "command" : "on", "device_identifier" : "satu"}[EOS]\nnyalakan oven di ruang tamu\n>\n'
