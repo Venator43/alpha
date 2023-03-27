2a9d328be06d2278e1e0e5b519f1516555728cb3
Fabian <38877113+Venator43@users.noreply.github.com>
2023-03-28 02:47:24 +0700

    finish type processor

--- type/basic_command.py
+++ type/basic_command.py
@@ -3,14 +3,21 @@ import json
 from . import command_interface
 
 class basic_command(command_interface.command_interface):
-	def __init__(self, API_KEY="", API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom", prompt):
-		API_URL = API_URL
-		headers = {"Authorization": f"Bearer {API_KEY}"}
-		prompt = prompt
+	def __init__(self, API_KEY="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", API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom", prompt="test"):
+		self.API_URL = API_URL
+		self.headers = {"Authorization": f"Bearer {API_KEY}"}
+		self.prompt = 'Mohon nyalakan lampu atas\n>{"device_type" : "lamp", "command" : "on", "device_identifier" : "atas"}[EOS]\nMatikan lampu kamar depan\n>{"device_type" : "lamp", "command" : "off", "device_identifier" : "kamar depan"}[EOS]\nMatikan lampu depan\n>{"device_type" : "lamp", "command" : "off", "device_identifier" : "depan"}[EOS]\nTolong nyalakan komputer di dapur\n>{"device_type" : "PC", "command" : "on", "device_identifier" : "dapur"}[EOS]\nNyalakan TV di tempat tidur\n>{"device_type" : "TV", "command" : "on", "device_identifier" : "Tempat Tidur"}[EOS]\nMatikan microwave dapur\n>{"device_type" : "microwave", "command" : "off", "device_identifier" : "dapur"}[EOS]\ntolong nyalakan device nomer satu\n>{"device_type" : "device", "command" : "on", "device_identifier" : "satu"}[EOS]\nnyalakan oven di ruang tamu\n>\n'
 
-	def query(payload):
-		response = requests.post(API_URL, headers=headers, json=payload)
+	def query(self, payload):
+		response = requests.post(self.API_URL, headers=self.headers, json=payload)
 		return response.json()
 
-	def predict(self, input):
-		return "test"
+	def predict(self, input_command):
+		model_input = self.prompt + input_command + "\n>"
+		output = self.query({
+		    "inputs": model_input,
+		    "parameters": {"do_sample" : False, "return_full_text" : False, "max_new_tokens" : 54}
+		})
+		output_dict = json.loads(output[0]["generated_text"].split("[EOS]")[0])
+		# print(output_dict)
+		return output_dict
--- type_processor.py
+++ type_processor.py
@@ -1,3 +1,25 @@
+import argparse
+import sys
+import pydoc
+
+import yaml
+from addict import Dict as Adict
+
 class type_processor:
-	def __init__(self):
-		continue
+	def __init__(self, config_path):
+		self.config_path = config_path
+		self.type_dict = {}
+		
+		with open(config_path) as configfile:
+			config = Adict(yaml.load(configfile, Loader=yaml.SafeLoader))
+		
+		analyzer_list = [t for t in config.type_list]
+		for i in analyzer_list:
+			type_name = i["type_name"]
+			type_object = i["type_object"]
+			type_parameter = i["type_param"]
+			cur_analyzer = pydoc.locate(type_object)(**type_parameter)
+			self.type_dict[type_name] = cur_analyzer
+
+	def run(self, command_type, command_input, **kwargs):
+		return self.type_dict[command_type].predict(command_input, **kwargs)
